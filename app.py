9f932037f533a060143e171b9e90d926cbacb2e5
ricardoduenez <63133331+ricardoduenez@users.noreply.github.com>
2020-10-13 19:05:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, escape, request, Response
 import random
+import hashlib
 
 app = Flask(__name__)
 
@@ -23,6 +24,11 @@ def prime_check(n):
 def json_response():
 	return jsonify(foo='bar', bat ='baz')
 
+@app.route('/md5/<string:word>')
+def MD5(word):
+
+	hash_obj = hashlib.md5(word.encode())
+	return jsonify(input=word, output=hash_obj.hexdigest())
 
 
 @app.route('/factorial/<int:n>')
@@ -37,7 +43,30 @@ def IsFactorial(n):
 		for i in range(1, n+1):
 			factorial = factorial*i
 		return jsonify(input=n, output=factorial)
+ 
 
+@app.route("/fibonacci/<int:n>")
+def fibonacci_num(n):
+    fibonacci = []
+    c1 = 0
+    c2 = 1
+    fib = 0
+    check = 0
+
+    if n < 0:
+        return jsonify(input=n, output="Error: Input must be a positive integer")
+    elif n == 0:
+        fibonacci = [0]
+    else:
+        while check == 0:
+            fib = c1 + c2
+            c2 = c1
+            c1 = fib
+            if fib <= n:
+                fibonacci.append(fib)
+            else:
+                check = 1
+    return jsonify(input=n, output=fibonacci)
 
 
 @app.route('/cat')
